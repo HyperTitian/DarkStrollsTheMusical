398c27f9b47d860f747d0f3dbcf680c6de7ac184
Joshua Reynolds <joshuare@mtu.edu>
2021-10-21 09:03:46 -0400

    Working Commit

--- DarkStrolls/Assets/Scripts/MainMenuHandler.cs
+++ DarkStrolls/Assets/Scripts/MainMenuHandler.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mainMenuPanel;
+    public GameObject newUserPanel;
+    public GameObject returningUserPanel;
+
+    public InputField newUserEmail_Input;
+    public InputField newUserPassword_Input;
+    public InputField returningUserEmail_Input;
+    public InputField returningUserPassword_Input;
+    
+    public string newUserEmail_Text;
+    public string newUserPassword_Text;
+    public string returningUserEmail_Text;
+    public string returningUserPassword_Text;
+
+    public void NewUserAction()
     {
-        
+        mainMenuPanel.SetActive(false);
+        newUserPanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReturningUserAction()
     {
-        
+        mainMenuPanel.SetActive(false);
+        returningUserPanel.SetActive(true);
+    }
+
+    public void CreateUserAction()
+    {
+        newUserEmail_Text = newUserEmail_Input.text;
+        newUserPassword_Text = newUserPassword_Input.text;
+    }
+    
+    public void LoginUserAction()
+    {
+        returningUserEmail_Text = returningUserEmail_Input.text;
+        returningUserPassword_Text = returningUserPassword_Input.text;
     }
 }
