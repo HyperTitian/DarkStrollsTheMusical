61948f8ea4ba21e6642fcf2334219340ea2a7b26
DaDarkWizard <danimask@ilc.edu>
2021-10-22 00:48:20 -0400

    Commented all database classes.

--- DarkStrollsAPI/Data/Message.cs
+++ DarkStrollsAPI/Data/Message.cs
@@ -7,15 +7,27 @@ using System.Threading.Tasks;
 
 namespace DarkStrollsAPI.Data
 {
+    /// <summary>
+    /// Message in the database.
+    /// </summary>
     public class Message
     {
+        /// <summary>
+        /// Unique Id of the message.
+        /// </summary>
         [Key]
         [Column("id", TypeName = "int")]
         public int Id { get; set; }
 
+        /// <summary>
+        /// User that created the message.
+        /// </summary>
         [Column("userid", TypeName = "int")]
         public int UserId { get; set; }
 
+        /// <summary>
+        /// Text of the message.
+        /// </summary>
         [Column("text", TypeName = "nvarchar(255)")]
         public string Text { get; set; } = "";
         
--- DarkStrollsAPI/Models/DarkDbContext.cs
+++ DarkStrollsAPI/Models/DarkDbContext.cs
@@ -7,23 +7,49 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DarkStrollsAPI.Models
 {
+    /// <summary>
+    /// Database context for the API.
+    /// </summary>
     public class DarkDbContext : DbContext
     {
+        /// <summary>
+        /// Connection string, loaded at Startup.
+        /// </summary>
         public static string? ConnectionString { get; set; }
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public DarkDbContext() : base()
         {
         }
 
+        /// <summary>
+        /// DbSet for messages in the database.
+        /// </summary>
         public DbSet<Message> Messages { get; set; } = null!;
+
+        /// <summary>
+        /// DbSet for users in the database.
+        /// </summary>
         public DbSet<User> Users { get; set; } = null!;
 
+        /// <summary>
+        /// Called when initializing this context.
+        /// Links entities to tables.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Message>().ToTable("messages");
             modelBuilder.Entity<User>().ToTable("users");
         }
 
+        /// <summary>
+        /// Called when creating with the default constructor.
+        /// Sets up connection string.
+        /// </summary>
+        /// <param name="optionsBuilder">DbContextOptionsBuilder to create options with.</param>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(ConnectionString);
