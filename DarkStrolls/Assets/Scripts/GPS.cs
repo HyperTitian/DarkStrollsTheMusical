822346ccaea8185dd0d350db757efae3233d7310
Joshua Reynolds <joshuare@mtu.edu>
2021-10-29 14:50:25 -0400

    Location API in

---
+++ DarkStrolls/Assets/Scripts/GPS.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class GPS : MonoBehaviour
+{
+    public static GPS Instance { set; get; }
+    
+    public float latitude;
+    public float longitude;
+
+    public Text debugText;
+    
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(GetLocation());
+    }
+
+    IEnumerator GetLocation()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GPS");
+            yield break;
+        }
+        
+        Input.location.Start();
+        int maxWait = 20;
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+        yield break;
+    }
+
+    public void Update()
+    {
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+            debugText.text = debugText.text + "R";
+        }
+    }
+}
