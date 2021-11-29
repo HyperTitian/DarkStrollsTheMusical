e6f0202368ebb9b9e1441f40b78abf7e5d308276
Daniel Masker <dtmasker@mtu.edu>
2021-11-28 21:05:54 -0500

    Added bonfires to the database.

---
+++ DarkStrollsAPI/Data/Bonfire.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DarkStrollsAPI.Data
+{
+    /// <summary>
+    /// A bonfire as seen in the database.
+    /// </summary>
+    public class Bonfire
+    {
+        [Key]
+        [Column("id", TypeName = "int")]
+        public int Id { get; set; }
+
+        [Column("name", TypeName = "varchar(255)")]
+        public string Name { get; set; } = null!;
+
+        [Column("longitude", TypeName = "float")]
+        public double Longitude { get; set; }
+
+        [Column("latitude", TypeName = "float")]
+        public double Latitude { get; set; }
+    }
+}
---
+++ DarkStrollsAPI/Data/Requests/GetBonfiresRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DarkStrollsAPI.Data.Requests
+{
+    /// <summary>
+    /// Request for getting the bonfires.
+    /// </summary>
+    public class GetBonfiresRequest
+    {
+        public float Longitude { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Range { get; set; }
+    }
+}
