f693c1eeed5bc6b79afb41635654c8e2e331914c
Joshua Reynolds <joshuare@mtu.edu>
2021-12-03 11:46:06 -0500

    Canvas buttons on mian game work now

--- DarkStrolls/Assets/Scripts/GameManager.cs
+++ DarkStrolls/Assets/Scripts/GameManager.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject leaveMessagePanel;
+
     public void GoBackToMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LeaveAMessage()
+    {
+        leaveMessagePanel.SetActive(true);
+    }
+
+    public void SubmitMessage()
+    {
+        leaveMessagePanel.SetActive(false);
     }
 }
